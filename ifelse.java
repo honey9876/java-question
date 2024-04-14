44754e62dfa6b3582fe626568367c34134b12aac
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-04-14 17:34:10 +0530

    iflese

--- ifelse.java
+++ ifelse.java
@@ -4,21 +4,13 @@ public class ifelse {
         int age = 22;
         if (age >= 18) {
             System.err.println("adult : deive, vote,collage ");
-
-            
         }
         if (age>13 && age<18) {
-            System.out.println("teenager");
-            
+            System.out.println("teenager"); 
         }
 
         else{
             System.out.println("not adult");
         }
-
-
-
-    }
-
-    
+    }    
 }

0e3ed09203f84a0d79a2e6646c8b856baf870a0b
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-04-22 12:39:56 +0530

    question basic

---
+++ helo.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void allOccurrences(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return;
+        }
+        if (arr[i] == key) {
+            System.out.println("Key found at index " + i);
+        }
+        allOccurrences(arr, key, i + 1);
+    }
+}

8bd381e7426f6520e37974b7a654a40aad77ba36
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-04-27 22:41:20 +0530

    if else quesstion

---
+++ report.java
@@ -0,0 +1,9 @@
+public class report {
+    
+    public static void main(String[] args) {
+        int marks = 76;
+
+        String reportcard = marks >= 33 ? "pass" : "Fail";
+        System.out.println(reportcard);
+    }
+}

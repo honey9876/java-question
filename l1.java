4b12b5331c7e5855fad51c592c70815953a5b919
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-05-16 06:23:19 +0530

    basic

---
+++ l1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class l1 {
+    public static void update(int maerk[]) {
+        for(int i=0 i<marks.length; i++) {
+            marks[i] = marks[i] = 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {98, 99, 100};
+        update(marks);
+
+        for(int i=0; i<marks.length; i++) {
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+        
+    }
+}
